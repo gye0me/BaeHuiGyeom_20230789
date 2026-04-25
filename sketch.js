7e6027cfbc369e32bd8492bc9de73e8bafff6fad
BaeHuiGyeom <heegum13@gmail.com>
2026-04-25 19:20:04 +0900

    적 추가(점 랜덤 못함)

--- sketch.js
+++ sketch.js
@@ -2,17 +2,31 @@ let px, py;
 let pd = 60; // 플레이어의 위치와 크기
 let isMoving = false;
 let dir = "RIGHT";
+let eSpeed = 5; // 적의 속도
 
-// 적의 위치와 크기
+let ex = [500, 1000, 1500, 2000, 2500]; // 적의 위치
+let ey = [500, 1000, 500, 1000, 500];
+let eSize = 5;
+
+
 let dx = [100, 200, 300, 400, 500];
 let dy = [150, 250, 100, 300, 200];
-let dSize = 28;
+let dSize = 30;
 let dActive = [true, true, true, true, true];
+let activeDotsCount = 0;
 
 let score = 0;
 let energy = 3;
 let gameOver = false;
 let gameWin = false;
+
+function randomizeDots() {
+  for(let i = 0; i < 5; i++) {
+    dx[i] = random(100, 2700);
+    dy[i] = random(100, 1400);
+    dActive[i] = true;
+  }
+}
 
 function setup() {
   createCanvas(2816, 1536);
@@ -23,38 +37,31 @@ function setup() {
 function draw() {
   background(0);
   isMoving = false;
+  activeDotsCount = 0;
 
 
   if (!gameOver && !gameWin) {
-    if(keyIsDown(LEFT_ARROW)) {
-      if (isHittingWall(px - 4, py)) {
-        px -= 4;  
-    }
-      isMoving = true;
-      dir = "LEFT";
-    }
-    if(keyIsDown(RIGHT_ARROW)) {
-      if (isHittingWall(px + 4, py)) {
-        px += 4; 
-      }
-      isMoving = true;
-      dir = "RIGHT";
-    }
-    if(keyIsDown(UP_ARROW)) {
-      if (isHittingWall(px, py - 4)) {
-        py -= 4; 
-      }
-      isMoving = true;
-      dir = "UP";
-    }
-    if(keyIsDown(DOWN_ARROW)) {
-      if (isHittingWall(px, py + 4)) {
-        py += 4; 
-      }
-      isMoving = true;
-      dir = "DOWN";
+    if(keyIsDown(LEFT_ARROW)) { if (!isHittingWall(px - 4, py)) px -= 4; isMoving = true; dir = "LEFT";}
+    if(keyIsDown(RIGHT_ARROW)) { if (!isHittingWall(px + 4, py)) px += 4; isMoving = true; dir = "RIGHT";}
+    if(keyIsDown(UP_ARROW)) { if (!isHittingWall(px, py - 4)) py -= 4; isMoving = true; dir = "UP";}
+    if(keyIsDown(DOWN_ARROW)) {if (!isHittingWall(px, py + 4)) py += 4; isMoving = true; dir = "DOWN";}
+  
+
+  for(let i = 0; i < 5; i++) {
+    fill(0, 0, 255);
+    rect(ex[i], ey[i], 40, 40);
+    ex[i] += random(-eSpeed, eSpeed);
+    ey[i] += random(-eSpeed, eSpeed);
+
+    let d =dist(px, py, ex[i], ey[i]);
+    if (d < (pd + 40) / 2) {
+      energy--;
+      ex[i] = random(200, 2500);
+      ey[i] = random(200, 1200);
+      if (energy <= 0) gameOver = true;
     }
   }
+}
 
   drawMap();
 
@@ -62,13 +69,13 @@ function draw() {
   noStroke();
   if (isMoving) {
     if (dir == "RIGHT") {
-    arc (px, py, pd, pd, 0.5, TWO_PI - 0.5);
+      arc (px, py, pd, pd, 0.5, TWO_PI - 0.5);
     } else if (dir == "LEFT") {
-    arc (px, py, pd, pd, PI + 0.5, PI - 0.5);
+      arc (px, py, pd, pd, PI + 0.5, PI - 0.5);
     } else if (dir == "UP") {
-    arc (px, py, pd, pd, -HALF_PI + 0.5, -HALF_PI - 0.5);
+      arc (px, py, pd, pd, -HALF_PI + 0.5, -HALF_PI - 0.5);
     } else if (dir == "DOWN") {
-    arc (px, py, pd, pd, HALF_PI + 0.5, HALF_PI - 0.5);
+      arc (px, py, pd, pd, HALF_PI + 0.5, HALF_PI - 0.5);
     }
   } else {
     ellipse(px, py, pd);
@@ -88,9 +95,32 @@ function draw() {
     }
   }
 
-  if (activeDotsCount == 0) {
-    gameWin = true;
+  if (activeDotsCount == 0) gameWin = true;
+    // 점수
+  fill(255);
+  textSize(80);
+  text("점수: " + score, 50, 80);
+  text("에너지: " + energy, 50, 180);
+
+  if (gameOver) {
+    fill(255, 0, 0);
+    textSize(120);
+    textAlign(CENTER);
+    text("GAME OVER", width / 2, height / 2);
+    textSize(60);
+    text("R키를 누르세요", width / 2, height / 2 + 100);
   }
+
+  if (gameWin) {
+    fill(0, 255, 0);
+    textSize(120);
+    textAlign(CENTER);
+    text("YOU WIN!", width / 2, height / 2);
+    textSize(60);
+    text("R키를 누르세요", width / 2, height / 2 + 100);
+  }
+  if (keyIsPressed && key == 'r') resetGame();
+}
 
   for(let i = 0; i < 5; i++) {
     if(dActive[i] == true) {
@@ -106,48 +136,20 @@ function draw() {
   }
 
 
-    // 점수
-      fill(255);
-      textSize(80);
-      text("점수: " + score, 50, 80);
-      text("에너지: " + energy, 50, 180);
-
     
-  if (gameOver) {
-    fill(255, 0, 0);
-    textSize(120);
-    textAlign(CENTER);
-    text("GAME OvER", width / 2, height / 2);
-    textSize(60);
-    text("R키를 누르세요", width / 2, height / 2 + 100);
-  }
+  
 
-  if (gameWin) {
-    fill(0, 255, 0);
-    textSize(120);
-    textAlign(CENTER);
-    text("YOU WIN!", width / 2, height / 2);
-    textSize(60);
-    text("R키를 누르세요", width / 2, height / 2 + 100);
-  }
-
-  textAlign(LEFT);
-
-  if (keyIsPressed && key == 'r') {
-    resetGame();
+function randomizeDots() {
+  for(let i = 0; i < 5; i++) {
+    dx[i] = random(200, 2500);
+    dy[i] = random(200, 1200);
+    dActive[i] = true;
   }
 }
 
-    
-    // 벽과 충돌 처리
-    let wallX = 1200;
-    if (px + pd/2 < wallX) {
-      px += 5;
-    } else if (px - pd/2 > wallX + 400) {
-      px -= 5;
-    }
 
-    function drawMap() {
+
+  function drawMap() {
     stroke(0, 200, 400);
     strokeWeight(9);
     noFill();
@@ -175,20 +177,11 @@ function draw() {
 
     function isHittingWall(nx, ny) {
       let r = (pd / 20) + 3;
-      if (nx - r < 100 || nx + r > 2716 || ny - r < 100 || ny + r > 1436) {
-        return false;
-      }
-      if (nx + r > 1200 && nx - r < 1600 && ny + r > 600 && ny - r < 900) {
-        return false;
-      }
-      if (nx + r > 300 && nx - r < 700 && ny + r > 300 && ny - r < 500) {
-        return false;
-      }
-      if (nx + r > 2100 && nx - r < 2500 && ny + r > 300 && ny - r < 500) {
-        return false;
-      }
-
-      return true;
+      if (nx - r < 100 || nx + r > 2716 || ny - r < 100 || ny + r > 1436) return true;
+      if (nx + r > 1200 && nx - r < 1600 && ny + r > 600 && ny - r < 900) return true;
+      if (nx + r > 300 && nx - r < 700 && ny + r > 300 && ny - r < 500)  return true;
+      if (nx + r > 2100 && nx - r < 2500 && ny + r > 300 && ny - r < 500) return true;
+      return false;
     }
     
     function resetGame() {
@@ -204,4 +197,3 @@ function draw() {
       }
     };
     
-
